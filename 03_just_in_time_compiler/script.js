b405088bf3a6cfc67118d77c3b1c80db47b43f03
David Kalmus <dejvios@seznam.cz>
2025-07-27 00:06:27 +0200

    advanced-js

--- 03_just_in_time_compiler/script.js
+++ 03_just_in_time_compiler/script.js
@@ -2,12 +2,6 @@
 Interpreter VS Compiler
 • Interpreter - řádek po řádku, rychlejší -> pomalejší (pak je pomalejší)
 • Compiler - celý kód najednou, pomalejší -> rychlejší 
-
-Just in time compiler (JIT)
-• spojení Interpreter a Compiler
-• je typ kompilátoru, který během běhu programu překládá JS kód (nebo jeho část) 
-  z AST nebo bytecode do strojového kódu, aby urychlil jeho vykonávání 
-  oproti čisté interpretaci
 */
 
 const sum = (num1, num2) => num1 + num2;
@@ -15,3 +9,11 @@ const sum = (num1, num2) => num1 + num2;
 for (let i = 0; i < 1000; i++) {
     sum(5, 10); 
 }
+
+/*
+Just in time compiler (JIT)
+• součástí JS enginu
+• je typ kompilátoru, který během běhu programu překládá JS kód (nebo jeho část) 
+  z AST nebo bytecode do strojového kódu, aby urychlil jeho vykonávání 
+  oproti čisté interpretaci
+*/
