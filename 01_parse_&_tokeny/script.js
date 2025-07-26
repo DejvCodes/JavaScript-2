f27a792247f940761a2a11565b442d82d59c1a8e
David Kalmus <dejvios@seznam.cz>
2025-07-26 23:48:48 +0200

    advanced-js

--- 01_parse_&_tokeny/script.js
+++ 01_parse_&_tokeny/script.js
@@ -1,1 +1,39 @@
-console.log("Hello")
+const x = 5 + 3;
+/*
+  const - klíčové slovo
+  x - identifikátor
+  = - operátor
+  5 - číslo
+  + - operátor
+  3 - číslo
+  ; - středník
+  rozparsování prvků na tzv. tokeny
+*/
+
+/*
+• JS engine - přeloží JS kód do nul a jedniček
+• JS je vysoký jazyk (en výrazy) - je srozumitelný
+• JS enginů je více - V8 (nejrychlejší - Google)
+                    - SpiderMonkey
+
+JS engine
+  1.Kód píšeme do souboru.js       4.Interpreter
+  2.Parsuje se                     5.Profiler
+  3.Abstract Syntax Tree (AST)     6.Compiler -> převede na jedničky a nuly
+
+Co je parsování?
+• rozparsování prvků na tzv. tokeny
+• proces převodu zdrojovéko kódu JS do AST
+
+Co je AST?
+• Abstract Syntax Tree 
+• stromová reprezentace struktury zdrojového kódu JavaScriptu, kterou
+  používají nástroje pro analýzu a transformaci kódu
+
+Co je Interpreter?
+• je program, který přímo vykoná JS kód řádek po řádku, často na základě AST, aniž
+  by ho předem překládal do strojového kódu
+
+Co je Compiler?
+• vezme celý kód a přeloží ho do jiného cílového jazyka např. do strojového kódu
+*/
---
+++ 02_interpreter_&_compiler/script.js
@@ -0,0 +1,16 @@
+// Interpreter VS Compiler
+
+/*
+Interpreter
+• překládá řádek po řádku
+• každý řádek přeložen a proveden
+• lépe se testuje (oprava chyby na daném řádku)
+• je pomalejší (překlad za běhu)
+
+VS
+
+Compiler
+• překládá celý kód
+• pokud je někde chyba, tak se celý kód zkompiluje znovu
+• je rychlejší
+*/
