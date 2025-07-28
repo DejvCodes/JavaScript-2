5895d45d4c3fde42c553cb0248decb3b52c2f5e4
David Kalmus <dejvios@seznam.cz>
2025-07-28 17:39:38 +0200

    advanced-js

---
+++ 12_execution_context/script.js
@@ -0,0 +1,43 @@
+/*
+Execution context
+• funkční kontext
+• globální kontext
+
+Funkční kontext
+• vzniká pokaždé, když je volána funkce
+• má vlastní scope a this, nezávislý na globálním kontextu
+
+Globální kontext
+• vytváří se automaticky při spuštění skriptu
+• obsahuje globální objekt (window v prohlížeči) a this
+*/
+
+// Funkční kontext
+const firstFunction = () => {
+    console.log('první funkce');   
+}
+
+const secondFunction = () => {
+    console.log('druhá funkce');   
+}
+
+const thirdFunction = () => {
+    console.log('třetí funkce');   
+}
+
+firstFunction();
+secondFunction();
+thirdFunction();
+
+// Globální kontext
+this.setInterval(() => {
+    console.log('test - this');
+}, 2000);
+
+window.setInterval(() => {
+    console.log('test - window');
+}, 2000);
+
+setInterval(() => {
+    console.log('test ');
+}, 2000);
