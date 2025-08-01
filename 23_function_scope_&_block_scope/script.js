ef0663b42d27d867207beea4c9c061bfd5d248c1
David Kalmus <dejvios@seznam.cz>
2025-08-01 20:47:56 +0200

    advanced-js

--- 23_function_scope_&_block_scope/script.js
+++ 23_function_scope_&_block_scope/script.js
@@ -27,3 +27,19 @@ Block scope
 • proměnné jsou dostupné pouze uvnitř bloku – tj. mezi { ... }
 • Platí pro let a const, ne pro var
 */
+
+// Oba dva scopy jsou lokální (blokové), protože jsou uvnitř if-bloků
+if (true) {
+  // první blokový scope (nadřazený)
+  const age1 = 23;
+
+  if (true) {
+    // druhý blokový scope (vnořený do prvního)
+    const age = 23;
+
+    console.log(age);  // přístupné – je ve stejném (vnitřním) scope
+    console.log(age1); // přístupné – je z nadřazeného (vnějšího) scope
+  }
+
+  // console.log(age); // ReferenceError – age není dostupné mimo vnitřní blok
+}
