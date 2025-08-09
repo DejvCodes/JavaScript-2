24b70313c07bbd5302d19175572c61082abb5d00
David Kalmus <dejvios@seznam.cz>
2025-08-09 18:39:59 +0200

    advanced-js

---
+++ 48_function_callable_oject/script.js
@@ -0,0 +1,30 @@
+// Funkce
+function four() {
+    console.log('test');
+}
+
+four.a = 10;
+four.b = 'string';
+
+console.log(four.a);
+console.log(four.b);
+four();
+
+/*
+Callable Object v JS
+• v JS se „callable object“ obvykle označuje funkce, protože funkce 
+  jsou objekty, které lze volat (jsou „callable“) – tj. mají [[Call]] 
+  interní metodu
+
+Základní definice:
+• callable object = objekt, který se chová jako funkce – lze ho zavolat pomocí ()
+
+Callable Object -> ukázka:
+
+const specialObject = {
+    a: 10,
+    b: 'string',
+    name: 'four',
+    ()): console.log('test')
+}
+*/
